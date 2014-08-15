e19ac24561e4ff8dfe45808af279e257ce42a957
hots0uce <jm.giuffrida@gmail.com>
2014-08-14 22:18:16 -0400

    map updates
    still need to do magical things like implement picking the start and
    adjacent stuff

--- src/map.js
+++ src/map.js
@@ -2,18 +2,44 @@ var dimensions = {
 		width: 150,
 		height: 150
 	},
-	map;
+	fillPercent = 0.5;
 
-exports.createArray = function() {
-	var i = 0;
-	map = new Array(dimensions.width);
+exports.generate = function() {
+	var i = 0, j,
+		map = new Array(dimensions.height),
+		wall = dimensions.width * dimensions.height,
+		floor = 0,
+		pos = { x: 0, y: 0 };
+
 	for( ; i<dimensions.width; i++) {
-		map[i] = new Array(dimensions.height);
+		for(j = 0; j<dimensions.height; j++) {
+			map[i][j] = 0;
+		}
+	}
+
+	pos = exports.pickStart();
+	map[pos.x][pos.y] = 1;
+	++floor;
+	while(floor/wall < fillPercent) {
+		pos = exports.pickAdjacent();
+		map[pos.x][pos.y] = 1;
+		++floor;
 	}
 
 	return map;
+
 };
 
-exports.generate = function() {
+exports.pickStart = function() {
+	return {
+		x: 0,
+		y: 0
+	};
+};
 
+exports.pickAdjacent = function(x,y) {
+	return {
+		x: 0,
+		y: 0
+	};
 };
